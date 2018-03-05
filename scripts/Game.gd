a5395e041afff51e2bf25d120393dcbbb319630d
Guilherme Oliveira <htoguilherme@gmail.com>
2018-03-05 04:58:05 -0300

    Punching trees

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -5,7 +5,7 @@ export(PackedScene) var trunk_scene
 onready var first_trunk_position = $FirstTrunkPosition
 
 var last_spawn_position
-
+var trunks = []
 
 func _ready():
 	last_spawn_position = first_trunk_position.position
@@ -17,6 +17,13 @@ func _spawn_first_trunks():
 		add_child(new_trunk)
 		new_trunk.position = last_spawn_position
 		last_spawn_position.y -= new_trunk.sprite_height
+		new_trunk.initialize_trunk(false, false)
+		trunks.append(new_trunk)
+	
+
+func punch_tree(from_right):
+	trunks[0].remove(from_right)
+	trunks.pop_front()
 
 
 
